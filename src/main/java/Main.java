f1cb2a5d327d3e785c9eaf5df6d8e4d89b16c1e2
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2024-01-28 17:39:26 +0200

    Add implementation to first case.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 	private static final double A = 2.3;
 	public double calculate(double x) {
-		return Math.pow(x - 2, 2) + 6 * A;
+		double result = 0;
+
+		if (x < 0.2 || x > 2.8) {
+			throw new IllegalArgumentException();
+		}
+
+		if (x <= 0.3) {
+			result = 1.5*2.3*Math.pow(Math.cos(x), 2);
+		} else if (x <= 2.3) {
+			result = Math.pow(x - 2, 2) + 6 * A;
+		} else {
+
+		}
+		return result;
 	}
 }
