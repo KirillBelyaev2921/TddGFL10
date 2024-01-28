9ecc3d5d8b66bc998c0126faaa16081b43c6a828
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2024-01-28 17:43:52 +0200

    Add implementation to third case and end item 3.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 public class Main {
 	private static final double A = 2.3;
 	public double calculate(double x) {
-		double result = 0;
+		double result;
 
 		if (x < 0.2 || x > 2.8) {
 			throw new IllegalArgumentException();
 		}
 
 		if (x <= 0.3) {
-			result = 1.5*2.3*Math.pow(Math.cos(x), 2);
+			result = 1.5*A*Math.pow(Math.cos(x), 2);
 		} else if (x <= 2.3) {
 			result = Math.pow(x - 2, 2) + 6 * A;
 		} else {
-
+			result = 3*A*Math.tan(x);
 		}
 		return result;
 	}
